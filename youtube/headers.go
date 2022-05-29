06b39ec8e01c62cb8de4dc7067b23052585d8f7a
alagha-go <abdihakimosman1480@gmail.com>
2022-05-29 17:50:17 +0000

    sets header for a given request

---
+++ youtube/headers.go
@@ -0,0 +1,15 @@
+package youtube
+
+import "net/http"
+
+
+type Header struct {
+	Key			string
+	Value		string
+}
+
+func SetHeaders(req *http.Request, client Client) {
+	for _, header := range client.Headers {
+		req.Header.Set(header.Key, header.Value)
+	}
+}
